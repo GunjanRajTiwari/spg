bd284cbed142839e1de50f4b7f18490961fa1526
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-11-21 16:18:28 +0545

    Changed bin-movement features (keys and speed)🎉

--- js/level1.js
+++ js/level1.js
@@ -5,19 +5,19 @@ var time = 59;
 var score = 0;
 
 document.addEventListener("keydown", event => {
-	if (event.key === "j" && binPos > 0) {
-		binPos -= 10;
-		bin.style.left = `${binPos}px`;
-	}
-	if (event.key === "k" && binPos < window.innerWidth - bin.clientWidth - 1) {
-		binPos += 10;
-		bin.style.left = `${binPos}px`;
-	}
+    if ((event.key === "ArrowLeft" || event.key === "a") && binPos > 0) {
+        binPos -= 15;
+        bin.style.left = `${binPos}px`;
+    }
+    if ((event.key === "ArrowRight" || event.key === "d") && binPos < window.innerWidth - bin.clientWidth - 1) {
+        binPos += 15;
+        bin.style.left = `${binPos}px`;
+    }
 });
 
 // Random generator
 function random() {
-	return Math.floor(Math.random() * (window.innerWidth - 60) + 4);
+    return Math.floor(Math.random() * (window.innerWidth - 60) + 4);
 }
 
 const timeCard = document.getElementById("time");
@@ -28,59 +28,59 @@ const popup = document.getElementById("popup");
 const playBtn = document.getElementById("play-btn");
 
 function play() {
-	time = 59;
-	score = 0;
-	scoreCard.innerText = score > 9 ? score : "0" + score;
-	var timer = setInterval(() => {
-		timeCard.innerText = time > 9 ? time : "0" + time;
-		if (time % 3 == 0) {
-			var waste = document.createElement("img");
-			waste.src = "../images/bag.png";
-			waste.classList.add("waste");
-			waste.style.left = `${random()}px`;
-			container.appendChild(waste);
-			rain(waste);
-		}
-		time -= 1;
-		if (time < 0) {
-			clearInterval(timer);
-			splash.style.display = "flex";
-			popup.innerHTML = `
+    time = 59;
+    score = 0;
+    scoreCard.innerText = score > 9 ? score : "0" + score;
+    var timer = setInterval(() => {
+        timeCard.innerText = time > 9 ? time : "0" + time;
+        if (time % 3 == 0) {
+            var waste = document.createElement("img");
+            waste.src = "../images/bag.png";
+            waste.classList.add("waste");
+            waste.style.left = `${random()}px`;
+            container.appendChild(waste);
+            rain(waste);
+        }
+        time -= 1;
+        if (time < 0) {
+            clearInterval(timer);
+            splash.style.display = "flex";
+            popup.innerHTML = `
             <h1>Game Over</h1>
             <p>
             Final Score : ${score}
             </p>
             <button id="play-btn" class="button">Play Again</button>
             `;
-			document.getElementById("play-btn").onclick = playgame;
-		}
-	}, 1000);
+            document.getElementById("play-btn").onclick = playgame;
+        }
+    }, 1000);
 }
 
 function rain(waste) {
-	const down = setInterval(() => {
-		waste.style.top = (waste.offsetTop += 2) + "px";
-		var currentHeight = waste.offsetTop + 50;
-		if (
-			currentHeight > window.innerHeight - 50 &&
-			waste.offsetLeft > bin.offsetLeft &&
-			waste.offsetLeft + 50 < bin.offsetLeft + 100
-		) {
-			clearInterval(down);
-			container.removeChild(waste);
-			score += 5;
-			tung.play();
-			scoreCard.innerText = score > 9 ? score : "0" + score;
-		}
-		if (waste.offsetTop > window.innerHeight - 50) {
-			clearInterval(down);
-		}
-	}, 8);
+    const down = setInterval(() => {
+        waste.style.top = (waste.offsetTop += 2) + "px";
+        var currentHeight = waste.offsetTop + 50;
+        if (
+            currentHeight > window.innerHeight - 50 &&
+            waste.offsetLeft > bin.offsetLeft &&
+            waste.offsetLeft + 50 < bin.offsetLeft + 100
+        ) {
+            clearInterval(down);
+            container.removeChild(waste);
+            score += 5;
+            tung.play();
+            scoreCard.innerText = score > 9 ? score : "0" + score;
+        }
+        if (waste.offsetTop > window.innerHeight - 50) {
+            clearInterval(down);
+        }
+    }, 8);
 }
 
 const playgame = () => {
-	splash.style.display = "none";
-	play();
+    splash.style.display = "none";
+    play();
 };
 
 playBtn.onclick = playgame;
